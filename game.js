caade9a60d2f0ddd3f937b35b59dc6d447d68261
Nelson <124norellana@frhsd.com>
2023-02-14 10:52:28 -0500

    snake, html, css, snake and game.js

---
+++ game.js
@@ -0,0 +1,33 @@
+import { update as updateSnake, draw as drawSnake,
+  SNAKE_SPEED } from "./snake.js";
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById('game-board');
+
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if(secondsSinceLastRender < 1 / SNAKE_SPEED)
+  {
+      return;
+  }
+  // console.log("RENDERING");
+  lastRenderTime = currentTime;
+      
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+// Update the snake and food
+function update()
+{
+  updateSnake();
+}
+
+// Draw/Render the snake and food
+function draw()
+{
+  drawSnake(gameBoard);
+}
---
+++ snake.js
@@ -0,0 +1,28 @@
+export const SNAKE_SPEED = 1;
+const snakeBody = [
+    { x : 10, y : 11 },
+    { x : 11, y : 11 },
+    { x : 12, y : 11}
+];    
+
+// Update the snake
+export function update()
+{
+    for (let i = snakeBody.length - 2; i>= 0; i--)
+        {
+            snakeBody[i + 1] = { ...snakeBody[i]};
+        }
+
+}
+
+// Draw/Render the snake
+export function draw(gameBoard)
+{
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = segment.x;
+        snakeElement.style.gridColumnStart = segment.y;
+        snakeElement.classList.add('snake');
+        gameBoard.appendChild(snakeElement);
+    })
+}
