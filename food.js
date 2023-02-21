84bf07f94809d20d08f2b60daffb7d2c7cb7c1bf
Nelson <124norellana@frhsd.com>
2023-02-21 10:01:25 -0500

    Food and Expand

--- food.js
+++ food.js
@@ -1,23 +1,28 @@
-let food = { x: 10, y: 1 }
-const EXPANSION_RATE = 1
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 5
 
-//Update the snake
-export function update()
-{
-if(onSnake(food)) {
+export function update() {
+  if (onSnake(food)) {
     expandSnake(EXPANSION_RATE)
-    food = { x:20, y: 10 };
+    food = getRandomFoodPosition()
+  }
 }
 
+export function draw(gameBoard) {
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
 }
 
-// Draw/Render the snake
-export function draw(gameBoard)
-{
-const foodElement = document.createElement('div');
-snakeElement.style.gridRowStart = food.y;
-snakeElement.style.gridColumnStart = food.x;
-snakeElement.classList.add('food');
-gameBoard.appendChild(foodElement);
-    }
+function getRandomFoodPosition() {
+  let newFoodPosition
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition()
+  }
+  return newFoodPosition
+}
--- game.js
+++ game.js
@@ -1,37 +1,41 @@
-import { update as updateSnake, draw as drawSnake,
-  SNAKE_SPEED } from "./snake.js";
-import { update as updateFood, draw as drawFood } from "./food.js"
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
-let lastRenderTime = 0;
-const gameBoard = document.getElementById('game-board');
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
-  window.requestAnimationFrame(main);
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-  if(secondsSinceLastRender < 1 / SNAKE_SPEED)
-  {
-      return;
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/'
+    }
+    return
   }
-  // console.log("RENDERING");
-  lastRenderTime = currentTime;
-      
-  update();
-  draw();
+
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
 }
 
-window.requestAnimationFrame(main);
+window.requestAnimationFrame(main)
 
-// Update the snake and food
-function update()
-{
-  updateSnake();
-  updateFood();
+function update() {
+  updateSnake()
+  updateFood()
+  checkDeath()
 }
 
-// Draw/Render the snake and food
-function draw()
-{
-  gameBoard.innerHTML = '';
-  drawSnake(gameBoard);
-  drawFood(gameBoard);
+function draw() {
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
 }
